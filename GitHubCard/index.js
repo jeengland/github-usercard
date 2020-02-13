154f137ffef792f10d7010d9afc20144d799fe99
unknown <jacob.e.england@gmail.com>
2020-02-13 16:39:29 -0600

    Completed MVP and one stretch

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,11 +3,28 @@
            https://api.github.com/users/<your name>
 */
 
-// axios
-//   .get('https://api.github.com/users/jeengland')
-//   .then(response => {
-//     console.log(response);
-//   })
+// API Call
+
+const cards = document.querySelector('.cards');
+
+axios
+  .get('https://api.github.com/users/jeengland')
+  .then(response => {
+    const newCard = cardCaptor(response.data);
+    cards.appendChild(newCard);
+    axios
+      .get(response.data.followers_url)
+      .then(response => {
+        response.data.forEach((follower) => {
+          axios
+            .get(`https://api.github.com/users/${follower.login}`)
+            .then(response => {
+              const followerCard = cardCaptor(response.data);
+              cards.appendChild(followerCard);
+            })
+        })
+      })
+  })
 
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
@@ -31,8 +48,6 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
-
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
@@ -53,8 +68,41 @@ const followersArray = [];
 
 */
 
-const cardCreator = (props) {
-
+const cardCaptor = (props) => {
+  // Create elements 
+  const card = document.createElement('div'),
+        img = document.createElement('img'),
+        info = document.createElement('div'),
+        name = document.createElement('h3'),
+        username = document.createElement('p'),
+        location = document.createElement('p'),
+        profile = document.createElement('p'),
+        profileLink = document.createElement('a'),
+        followers = document.createElement('p'),
+        following = document.createElement('p'),
+        bio = document.createElement('p');     
+  // Add classes to appropriate elements
+  card.classList.add('card');
+  info.classList.add('card-info');
+  name.classList.add('name');
+  username.classList.add('username'); 
+  // Insert content into elements
+  img.src = props.avatar_url;
+  name.textContent = props.name;
+  username.textContent = props.login;
+  location.textContent = `Location: ${props.location}`;
+  profile.textContent = 'Profile: '
+  profileLink.textContent = props.html_url;
+  profileLink.href = props.url;
+  followers.textContent = `Followers: ${props.followers}`;
+  following.textContent = `Following: ${props.following}`;
+  bio.textContent = `Bio: ${props.bio}`;
+  // Append elements to each other in proper order
+  profile.append(profileLink);
+  info.append(name, username, location, profile, followers, following, bio);
+  card.append(img, info);
+  // Return card object   
+  return card;
 }
 
 /* List of LS Instructors Github username's:
